e56694bb4a8262eb95ded29ce767d81722738758
songhaibo <songhaibo@winzct.com>
2020-10-13 11:10:40 +0800

    添加了子模块服务提供者

---
+++ cloud-provider-payment8001/src/main/java/com/shb/springcloud/PaymentMain8001.java
@@ -0,0 +1,15 @@
+package com.shb.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author songhaibo
+ * @create 2020-10-13 11:06 上午
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
