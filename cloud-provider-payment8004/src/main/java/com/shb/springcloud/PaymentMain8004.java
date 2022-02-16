31f489388b2430820ce14b72dfe3312b2ec59d11
ext.songhaibo <ext.songhaibo@jd.com>
2022-02-16 16:56:30 +0800

    feat: add zookeeper作为注册中心

--- cloud-provider-payment8004/src/main/java/com/shb/springcloud/PaymentMain8004.java
+++ cloud-provider-payment8004/src/main/java/com/shb/springcloud/PaymentMain8004.java
@@ -1,4 +1,17 @@
 package com.shb.springcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author songhaibo
+ * @create 2022-02-16 11:06 上午
+ */
+@SpringBootApplication
+@EnableDiscoveryClient //该注解用于向zookeeper注册服务
 public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
 }
---
+++ cloud-provider-payment8004/src/main/java/com/shb/springcloud/controller/PaymentController.java
@@ -0,0 +1,25 @@
+package com.shb.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author songhaibo
+ * @create 2020-10-13 4:43 下午
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "payment/zk")
+    public String paymentZk() {
+        return "SpringCloud with zookeeper:" + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
