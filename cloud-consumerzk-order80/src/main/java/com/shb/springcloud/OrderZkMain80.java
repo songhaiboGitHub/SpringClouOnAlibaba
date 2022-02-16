852194401306cf0d0d4ea86ebbdbafdda19eb9f7
ext.songhaibo <ext.songhaibo@jd.com>
2022-02-16 17:19:17 +0800

    feat: add zookeeper作为注册中心

---
+++ cloud-consumerzk-order80/src/main/java/com/shb/springcloud/OrderZkMain80.java
@@ -0,0 +1,17 @@
+package com.shb.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author songhaibo
+ * @create 2022-02-14 10:13 上午
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZkMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZkMain80.class, args);
+    }
+}
