f40ca65a571a6e1663dafd020e9e7a32cd592c3d
ext.songhaibo <ext.songhaibo@jd.com>
2022-02-25 16:44:39 +0800

    feat: 自定义随机算法负载

---
+++ cloud-consumer-order80/src/main/java/com/shb/myrule/MySelfRule.java
@@ -0,0 +1,18 @@
+package com.shb.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MySelfRule {
+    @Bean
+    public IRule myRule() {
+//        return new RandomRule();//这一步就是替换Ribbon默认的轮询算法，改为随机算法负载
+
+        /**
+         * 依旧轮询策略，加上新需求，每台服务器调用5次。以前是每台服务器访问一次
+         */
+        return new RandomRule_ZY();//自定义负载均衡算法，每台服务其访问5次，就切下一台
+    }
+}
