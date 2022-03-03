e4a8de8615086ab26c23092f3503b022103e21a5
ext.songhaibo <ext.songhaibo@jd.com>
2022-03-03 15:25:40 +0800

    feat: 添加OpenFeign消费者

---
+++ cloud-consumer-feign-order80/src/main/java/com/shb/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,24 @@
+package com.shb.springcloud.controller;
+
+import com.shb.springcloud.entities.CommonResult;
+import com.shb.springcloud.entities.Payment;
+import com.shb.springcloud.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderFeignController {
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> get(@PathVariable("id") Long id) {
+        return paymentFeignService.get(id);
+    }
+
+}
---
+++ cloud-consumer-feign-order80/src/main/java/com/shb/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,16 @@
+package com.shb.springcloud.service;
+
+import com.shb.springcloud.entities.CommonResult;
+import com.shb.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Service
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult<Payment> get(@PathVariable("id") Long id);
+}
