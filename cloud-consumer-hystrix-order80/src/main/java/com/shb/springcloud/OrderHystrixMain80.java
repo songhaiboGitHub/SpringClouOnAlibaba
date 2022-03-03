ec2ed44a108733de9779be2d5d9ac22596adb430
ext.songhaibo <ext.songhaibo@jd.com>
2022-03-03 21:09:36 +0800

    feat: 增加Hystrix消费者80

---
+++ cloud-consumer-hystrix-order80/src/main/java/com/shb/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,20 @@
+package com.shb.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author zzyy
+ * @date 2020/02/18 17:20
+ **/
+@SpringBootApplication
+@EnableFeignClients
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class, args);
+    }
+}
+
+
+         
---
+++ cloud-consumer-hystrix-order80/src/main/java/com/shb/springcloud/controller/OrderHystrixController.java
@@ -0,0 +1,26 @@
+package com.shb.springcloud.controller;
+
+import com.shb.springcloud.service.PaymentHystrixService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderHystrixController {
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
+
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
+        return paymentHystrixService.paymentInfo_OK(id);
+    }
+
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        return paymentHystrixService.paymentInfo_TimeOut(id);
+    }
+}
