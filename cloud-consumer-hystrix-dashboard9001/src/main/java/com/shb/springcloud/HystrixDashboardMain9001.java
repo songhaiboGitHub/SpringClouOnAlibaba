d21087c861bd87132af74eae5eead8570173b3b4
songhaibo <songhaibo@winzct.com>
2022-03-06 15:29:32 +0800

    feat: 增加HystrixDashboard豪猪哥监控

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/shb/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.shb.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @authorsonghaibo
+ * @description
+ * @date2022-03-06下午2:55
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class);
+    }
+}
