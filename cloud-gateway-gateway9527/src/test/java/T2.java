8bef0fb17dd203c350129e73f46ac3dcf9af50d3
ext.songhaibo <ext.songhaibo@jd.com>
2022-03-10 18:27:45 +0800

    feat: 增加断言

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,10 @@
+import java.time.ZonedDateTime;
+
+public class T2 {
+
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+        //2022-03-10T18:10:28.888+08:00[Asia/Shanghai]
+    }
+}
