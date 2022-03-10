5e40ea288db91e82c1bbdd3b8555ab1dc8a950b0
ext.songhaibo <ext.songhaibo@jd.com>
2022-03-10 18:00:10 +0800

    feat: 增加Gateway动态路由
    三大核心：
    ● Route(路由)：路由是构建网关的基本模块，它由ID，目标URI，一系列的断言和过滤器组成，如果断言为true则匹配该路由
    ● Predicate(断言)：参考的是java8的java.util.function.Predicate开发人员可以匹配HTTP请求中的所有内容（例如请求头或请求参数），如果请求与断言相匹配则进行路由
    ● Filter(过滤)：指的是Spring框架中GatewayFilter的实例，使用过滤器，可以在请求被路由前或者之后对请求进行修改。

--- cloud-provider-payment8002/src/main/java/com/shb/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/shb/springcloud/controller/PaymentController.java
@@ -30,40 +30,46 @@ public class PaymentController {
     //获取微服务的信息
     private DiscoveryClient discoveryClient;
 
-    @PostMapping(value="/payment/create")
+    @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("*********插入结果：" + result);
 
         if (result > 0) {
-            return new CommonResult(200, "success serverPort:"+serverPort, result);
+            return new CommonResult(200, "success serverPort:" + serverPort, result);
         } else {
-            return new CommonResult(444, "fail serverPort:"+serverPort, result);
+            return new CommonResult(444, "fail serverPort:" + serverPort, result);
         }
     }
 
-    @GetMapping(value="/payment/get/{id}")
+    @GetMapping(value = "/payment/get/{id}")
     public CommonResult getPaymentById(@PathVariable("id") Long id) {
         Payment payment = paymentService.getPaymentById(id);
 
         log.info("*********查询结果：" + payment);
 
-        if (payment !=null) {
-            return new CommonResult(200, "success serverPort:"+serverPort, payment);
+        if (payment != null) {
+            return new CommonResult(200, "success serverPort:" + serverPort, payment);
         } else {
-            return new CommonResult(444, "fail serverPort:"+serverPort, null);
+            return new CommonResult(444, "fail serverPort:" + serverPort, null);
         }
     }
-    @GetMapping(value="/payment/discovery")
-    public Object discovery(){
+
+    @GetMapping(value = "/payment/lb")
+    public String lb() {
+        return serverPort;
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
         //获取微服务的信息
         List<String> services = discoveryClient.getServices();
         for (String service : services) {
-            log.info("****element:"+service);
+            log.info("****element:" + service);
         }
         List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
         for (ServiceInstance instance : instances) {
-            log.info(instance.getServiceId()+"\t"+instance.getPort()+"\t"+instance.getUri());
+            log.info(instance.getServiceId() + "\t" + instance.getPort() + "\t" + instance.getUri());
         }
         return this.discoveryClient;
     }
