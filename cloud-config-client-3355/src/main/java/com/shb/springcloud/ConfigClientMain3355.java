117d889165c098c0b060d7f415f5dfc9928ac4f9
ext.songhaibo <ext.songhaibo@jd.com>
2022-03-11 18:46:20 +0800

    feat: 增加config 3355

---
+++ cloud-config-client-3355/src/main/java/com/shb/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,14 @@
+package com.shb.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
+
+
+          
