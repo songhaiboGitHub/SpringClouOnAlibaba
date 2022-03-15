f669f79d6a6ec06b7578d1a5455eed1a1a9c1e6c
ext.songhaibo <ext.songhaibo@jd.com>
2022-03-15 10:34:19 +0800

    feat: 增加stream 消息驱动  消费者 8802

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/shb/springcloud/StreamMQMain8802.java
@@ -0,0 +1,15 @@
+package com.shb.springcloud;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zzyy
+ * @create 2020/2/17 21:13
+ **/
+
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
