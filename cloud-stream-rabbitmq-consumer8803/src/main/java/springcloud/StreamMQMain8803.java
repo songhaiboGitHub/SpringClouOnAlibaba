65c43a203dcbd3407929b2095945dcaaf6cc7a77
ext.songhaibo <ext.songhaibo@jd.com>
2022-03-15 10:53:26 +0800

    feat: 增加stream 消息驱动  消费者 8803  两个消费者公用同一个消费者组 避免重复消费

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/springcloud/StreamMQMain8803.java
@@ -0,0 +1,15 @@
+package springcloud;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zzyy
+ * @create 2020/2/17 21:13
+ **/
+
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
