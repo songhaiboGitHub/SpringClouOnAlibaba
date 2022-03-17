0ccba4608178ed36b112521ad4238755d74e14c0
ext.songhaibo <ext.songhaibo@jd.com>
2022-03-17 18:42:33 +0800

    feat: 增加Sentinel 8401 Sentinel流控 排队等待 直接失败

--- cloudalibaba-sentinel-service8401/src/main/java/com/shb/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/shb/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,10 +1,12 @@
 package com.shb.springcloud.alibaba.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@Slf4j
 public class FlowLimitController {
     @GetMapping("/testA")
     public String testA() {
@@ -13,7 +15,7 @@ public class FlowLimitController {
 
     @GetMapping("/testB")
     public String testB() {
-
+        log.info(Thread.currentThread().getName() + "lt" + " .... testB");
         return "------testB";
     }
 }
