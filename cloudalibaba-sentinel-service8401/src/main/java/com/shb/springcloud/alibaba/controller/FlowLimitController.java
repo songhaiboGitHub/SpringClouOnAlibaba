510d2111d770f3bfed26044ef043e44e961744d0
ext.songhaibo <ext.songhaibo@jd.com>
2022-03-17 10:49:23 +0800

    feat: 增加Sentinel 8401

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/shb/springcloud/alibaba/controller/FlowLimitController.java
@@ -0,0 +1,19 @@
+package com.shb.springcloud.alibaba.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+
+        return "------testB";
+    }
+}
+
