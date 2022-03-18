eb5549154653e55de971ce862fdc55b34d3a54ee
ext.songhaibo <ext.songhaibo@jd.com>
2022-03-18 15:25:28 +0800

    feat: 增加Sentinel 8401 Sentinel热点key

--- cloudalibaba-sentinel-service8401/src/main/java/com/shb/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/shb/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.shb.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -56,6 +59,22 @@ public class FlowLimitController {
         log.info("testE 测试异常数");
         int age = 10 / 0;
         return "------testE 测试异常数";
+    }
+
+
+    @GetMapping("/testHotKey")
+    //类似与@HystrixCommand  blockHandler失败兜底，fallBack
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        //int age = 10/0;
+        return "------testHotKey";
+    }
+
+
+    //兜底方法
+    public String deal_testHotKey(String p1, String p2, BlockException exception) {
+        return "------deal_testHotKey,o(╥﹏╥)o";
     }
 
 
