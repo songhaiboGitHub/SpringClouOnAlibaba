2671e3bf2734f009d8b8eb4ca44b76c680e505dc
ext.songhaibo <ext.songhaibo@jd.com>
2022-03-18 14:43:02 +0800

    feat: 增加Sentinel 8401 Sentinel降级 testD 测试RT

--- cloudalibaba-sentinel-service8401/src/main/java/com/shb/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/shb/springcloud/alibaba/controller/FlowLimitController.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 
 @RestController
 @Slf4j
@@ -18,5 +20,19 @@ public class FlowLimitController {
         log.info(Thread.currentThread().getName() + "lt" + " .... testB");
         return "------testB";
     }
+
+    @GetMapping("/testD")
+    public String testD() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("testD 测试RT");
+
+        return "------testD";
+    }
+
+
 }
 
